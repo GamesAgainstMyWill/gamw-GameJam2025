b9da52684068901f04b2ce376878b122c6ce4f28
GamesAgainstMyWill <makinggamesagainstmywill@gmail.com>
2025-01-25 23:44:39 -0500

    Updated sprites, added enemy pathfinding

--- gamejam2025/Scripts/little_crab.gd
+++ gamejam2025/Scripts/little_crab.gd
@@ -1,11 +1,25 @@
-extends Node2D
+extends CharacterBody2D
 
+const SPEED = 50
 
-# Called when the node enters the scene tree for the first time.
+@export var player: Node2D
+@onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
+
 func _ready():
 	$AnimatedSprite2D.play("walkin_small")
 
+func _physics_process(delta):
+	if player.global_position.x < global_position.x:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.flip_h = false
+	
+	var dir = to_local(nav_agent.get_next_path_position()).normalized()
+	velocity = dir * SPEED
+	move_and_slide()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func make_path():
+	nav_agent.target_position = player.global_position
+
+func _on_timer_timeout():
+	make_path()
